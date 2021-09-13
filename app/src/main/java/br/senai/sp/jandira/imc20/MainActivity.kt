55cb76e355372eceb6eac2feee593cff17b20d75
BRUNO RIBEIRO DE OLIVEIRA <21193381@CFP127ALUNO.LOCAL>
2021-09-13 15:59:13 -0300

    projeto atualizado

--- app/src/main/java/br/senai/sp/jandira/imc20/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc20/MainActivity.kt
@@ -2,11 +2,33 @@ package br.senai.sp.jandira.imc20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // var = variavel || val = constante
+
+
+        // criando as instancias das views
+        // para que possam ser manipuladas no kotlin
+        val editTxtPeso = findViewById<EditText>(R.id.edit_peso)
+        val txtAltura = findViewById<EditText>(R.id.edit_altura)
+        val button = findViewById<Button>(R.id.button_calcular)
+        val resultadoImc = findViewById<TextView>(R.id.text_view_resultado)
+
+        //adicionar um oouvinte de click no botao
+        button.setOnClickListener {
+
+            var peso = editTxtPeso.text.toString().toInt
+
+
+        }
+
+
     }
-    
 }
